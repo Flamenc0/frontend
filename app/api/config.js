21dc4eb3835553a59691d7cb5ea78ad47fce5dcb
Ivan <149413900+Flamenc0@users.noreply.github.com>
2024-05-30 08:49:11 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://saintedlittlebackend.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://flamenco-backend.nomorepartiesco.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
